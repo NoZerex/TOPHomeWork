ea487af9d50123a00c81903d82f14a6f17923eac
Сташевский Вадим <nozerex.st@mail.ru>
2024-09-09 20:16:35 +0300

    HomeWork 5-6

---
+++ src/HomeWork.java
@@ -0,0 +1,20 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        byte firstPrimitive = 123;
+        short secondPrimitive = 12345;
+        int thirdPrimitive = 1234567890;
+        long fourthPrimitive = 12345678900000L;
+        float fifthPrimitive = 123.45f;
+        double sixthPrimitive = 123456789.123456789;
+        char seventhPrimitive = 'a';
+        boolean eighthPrimitive = true;
+
+        int sum = 2 + 2;
+        int subtraction = 4 - 2;
+        int multiplications = 2 * 2;
+        int division = 4 / 2;
+        int dividingByTheRemainder = 20 % 10;
+        System.out.println(sum + " " + subtraction + " " + multiplications + " " + division + " " + dividingByTheRemainder);
+    }
+
+}
