666f7df861c0d6c39ed1fd80b444ca3a1b4d3222
Сташевский Вадим <nozerex.st@mail.ru>
2024-09-16 20:22:58 +0300

    HomeWork 11-12

--- src/SecondHw/SecondHomeWork.java
+++ src/SecondHw/SecondHomeWork.java
@@ -9,12 +9,12 @@ public class SecondHomeWork {
         double newDianaSalary = dianaSalary + ((dianaSalary * salaryIncrease) / 100);
         double newAndreySalary = andreySalary + ((andreySalary * salaryIncrease) / 100);
         double newMariaSalary = mariaSalary + ((mariaSalary * salaryIncrease) / 100);
-        double dianaSalarySalaryDifference = (newDianaSalary * 12) - (dianaSalary * 12);
-        double andreySalarySalaryDifference = (newAndreySalary * 12) - (andreySalary * 12);
-        double mariaSalarySalaryDifference = (newMariaSalary * 12) - (mariaSalary * 12);
-        System.out.println("Диана теперь получает " + newDianaSalary + "рублей. Годовой доход вырос на " + dianaSalarySalaryDifference + "рублей»");
-        System.out.println("Андрей теперь получает " + newAndreySalary + "рублей. Годовой доход вырос на " + andreySalarySalaryDifference + "рублей»");
-        System.out.println("Мария теперь получает " + newMariaSalary + "рублей. Годовой доход вырос на " + mariaSalarySalaryDifference + "рублей»");
+        double dianaSalaryDifference = (newDianaSalary * 12) - (dianaSalary * 12);
+        double andreySalaryDifference = (newAndreySalary * 12) - (andreySalary * 12);
+        double mariaSalaryDifference = (newMariaSalary * 12) - (mariaSalary * 12);
+        System.out.println("Диана теперь получает " + newDianaSalary + "рублей. Годовой доход вырос на " + dianaSalaryDifference + "рублей»");
+        System.out.println("Андрей теперь получает " + newAndreySalary + "рублей. Годовой доход вырос на " + andreySalaryDifference + "рублей»");
+        System.out.println("Мария теперь получает " + newMariaSalary + "рублей. Годовой доход вырос на " + mariaSalaryDifference + "рублей»");
         System.out.println(newDianaSalary);
     }
 }
