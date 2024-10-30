17d32cef73f6f534fd5f792692d5e704fdb780a0
MyUser <nozerex.st@mail.ru>
2024-10-30 23:28:24 +0500

    HomeWork 33-34

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    void makeSound() {
+        System.out.println("MakeSound");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Bird("Яго", true);
+        Animal animal2 = new Dog("Шарик");
+        Animal animal3 = new Cat("Матроскин");
+
+        Animal[] animals = {animal1, animal2, animal3};
+
+        for (Animal animal : animals) {
+            System.out.print(animal.getName() + ": ");
+            animal.makeSound();
+        }
+    }
+}
