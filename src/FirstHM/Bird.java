5eff330b05869f3c73d546d4f88b4db4ed4ee68d
MyUser <nozerex.st@mail.ru>
2024-11-06 21:07:02 +0500

    HomeWork 37-38

---
+++ src/FirstHM/Bird.java
@@ -0,0 +1,27 @@
+package FirstHM;
+
+public final class Bird extends Animal {
+    private boolean canFly;
+
+    public Bird(String name, boolean canFly) {
+        super(name);
+        this.canFly = canFly;
+    }
+
+    @Override
+    void move() {
+        if (canFly) {
+            System.out.println("Птица летает");
+        } else {
+            System.out.println("Птица ходит");
+        }
+
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Чирик-чирик.");
+    }
+
+
+}
